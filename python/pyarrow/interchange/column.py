13a65a662ca4f5d93bf8be613cf10c6113030bdf
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-07-20 17:18:42 +0200

    Fix `invalid-return-type` error

--- python/pyarrow/interchange/column.py
+++ python/pyarrow/interchange/column.py
@@ -379,7 +379,7 @@ class _PyArrowColumn:
             return ColumnNullType.USE_BITMASK, 0
 
     @property
-    def null_count(self) -> int:
+    def null_count(self) -> int | None:
         """
         Number of null elements, if known.
 
@@ -390,7 +390,7 @@ class _PyArrowColumn:
         return n
 
     @property
-    def metadata(self) -> Dict[str, Any]:
+    def metadata(self) -> None:
         """
         The metadata for the column. See `DataFrame.metadata` for more details.
         """
@@ -466,7 +466,7 @@ class _PyArrowColumn:
 
     def _get_data_buffer(
         self,
-    ) -> Tuple[_PyArrowBuffer, Any]:  # Any is for self.dtype tuple
+    ) -> Tuple[_PyArrowBuffer, Any] | None:  # Any is for self.dtype tuple
         """
         Return the buffer containing the data and the buffer's
         associated dtype.
@@ -505,7 +505,7 @@ class _PyArrowColumn:
                 "There are no missing values so "
                 "does not have a separate mask")
 
-    def _get_offsets_buffer(self) -> Tuple[_PyArrowBuffer, Any]:
+    def _get_offsets_buffer(self) -> Tuple[_PyArrowBuffer, Any] | None:
         """
         Return the buffer containing the offset values for variable-size binary
         data (e.g., variable-length strings) and the buffer's associated dtype.
