68b9347b821bd4518215e8b7fbea33ec43b12b24
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-07-24 19:29:00 +0100

    GH-20: [Python] Fix `unsupported-reference` typing (#24)
    * Add py.typed file to signify that the library is typed
    
    See the relevant PEP https://peps.python.org/pep-0561
    
    * Prepare `pyarrow-stubs` for history merging
    
    MINOR: [Python] Prepare `pyarrow-stubs` for history merging
    
    Co-authored-by: ZhengYu, Xu <zen-xu@outlook.com>
    
    * Add `ty` configuration and suppress error codes
    
    * One line per rule
    
    * Add licence header from original repo for all `.pyi` files
    
    * Revert "Add licence header from original repo for all `.pyi` files"
    
    This reverts commit 1631f3916479ce9e1fd7df1194f61cb420962fd5.
    
    * Prepare for licence merging
    
    * Exclude `stubs` from `rat` test
    
    * Add Apache licence clause to `py.typed`
    
    * Reduce list
    
    * Add `ty` as a step in the action
    
    * Run in the correct directory
    
    * Remove `check` from `pip`
    
    * Fix `unresolved-reference` error
    
    * Revert "Fix `unresolved-reference` error"
    
    This reverts commit 7ee3d2f04b2a01a2f1dacccc6d0995f48a3d374d.
    
    * Fix `unresolved-reference` error
    
    ---------
    
    Co-authored-by: Rok Mihevc <rok@mihevc.org>

--- python/stubs/__lib_pxi/types.pyi
+++ python/stubs/__lib_pxi/types.pyi
@@ -29,7 +29,6 @@ from .io import Buffer
 from .scalar import ExtensionScalar
 
 _AsPyType = TypeVar("_AsPyType")
-_DataTypeT = TypeVar("_DataTypeT", bound=DataType)
 
 class _Weakrefable: ...
 class _Metadata(_Weakrefable): ...
@@ -185,6 +184,8 @@ class DataType(_Weakrefable):
             A valid PyCapsule with name 'arrow_schema' containing an
             ArrowSchema pointer.
         """
+
+_DataTypeT = TypeVar("_DataTypeT", bound=DataType)
 
 class _BasicDataType(DataType, Generic[_AsPyType]): ...
 class NullType(_BasicDataType[None]): ...
