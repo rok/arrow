419691cfe73ca7423315440296157e75dce5caed
Rok Mihevc <rok@mihevc.org>
2026-01-23 19:36:33 +0100

    benchmark change

--- cpp/src/arrow/io/memory_benchmark.cc
+++ cpp/src/arrow/io/memory_benchmark.cc
@@ -23,7 +23,6 @@
 #include "arrow/util/cpu_info.h"
 #include "arrow/util/simd.h"
 
-#include "arrow/util/benchmark_util.h"
 #include "benchmark/benchmark.h"
 
 namespace arrow {
@@ -263,7 +262,7 @@ static void MemoryBandwidth(benchmark::State& state) {  // NOLINT non-const refe
 }
 
 #    ifdef ARROW_HAVE_SSE4_2
-static void SetCacheBandwidthArgs(arrow::BenchmarkType* bench) {
+static void SetCacheBandwidthArgs(benchmark::internal::Benchmark* bench) {
   auto cache_sizes = {kL1Size, kL2Size, kL3Size};
   for (auto size : cache_sizes) {
     bench->Arg(size / 2);
@@ -277,7 +276,7 @@ static void SetCacheBandwidthArgs(arrow::BenchmarkType* bench) {
 BENCHMARK_TEMPLATE(MemoryBandwidth, Read)->Apply(SetCacheBandwidthArgs);
 #    endif  // ARROW_HAVE_SSE4_2
 
-static void SetMemoryBandwidthArgs(arrow::BenchmarkType* bench) {
+static void SetMemoryBandwidthArgs(benchmark::internal::Benchmark* bench) {
   // `UseRealTime` is required due to threads, otherwise the cumulative CPU time
   // is used which will skew the results by the number of threads.
   bench->Arg(kMemoryPerCore)->ThreadRange(1, kNumCores)->UseRealTime();
